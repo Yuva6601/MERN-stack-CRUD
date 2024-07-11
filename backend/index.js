7ab77ef4913f92cdbd598879ff856bb1222b7ebc
yuvanesh <yuvanesh@hexrfactory.com>
2024-07-11 13:30:05 +0530

    push thru

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
+const cors = require('cors')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send("Hello hey")
@@ -9,6 +17,12 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log('App is listening to the port', port);
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(port, () => {
+            console.log('App is listening to the port', port);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
